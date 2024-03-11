9af76931972365a6d24516daec3e91bdb54f8be4
Jack Saunders <36443883+jsaunders909@users.noreply.github.com>
2024-03-11 16:24:09 +0000

    Added Dockerfile launcher

--- start_docker.sh
+++ start_docker.sh
@@ -1,1 +1,7 @@
-docker run -it --gpus $1 -v /mnt/faster0/jrs68/wav2lip:/workspace -it --entrypoint "/bin/bash" --shm-size=256gb -t jrs68/wav2lip
+if [ "$1" = "" ]
+then
+  echo "Usage: $1 "
+  exit
+fi
+
+hare run -it --gpus $1 -v /mnt/faster0/jrs68/wav2lip:/workspace -it --entrypoint "/bin/bash" --shm-size=256gb -t jrs68/wav2lip
