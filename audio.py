2574772d23900d01e9fa7779ae207855a6d4e49a
Jack Saunders <36443883+jsaunders909@users.noreply.github.com>
2024-03-15 10:18:15 +0000

    UNET Added

--- audio.py
+++ audio.py
@@ -46,6 +46,7 @@ def linearspectrogram(wav):
     return S
 
 def melspectrogram(wav):
+    print(f'Wav shape = {wav.shape}')
     D = _stft(preemphasis(wav, hp.preemphasis, hp.preemphasize))
     S = _amp_to_db(_linear_to_mel(np.abs(D))) - hp.ref_level_db
     
